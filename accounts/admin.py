6b972f4cee5d0156dc11e028ceb05d4f34b324f9
Aizen <eltoncambulo@gmail.com>
2025-07-03 05:31:14 -0300

    admin web laod

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import CustomUserModel
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUserModel
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = ['username', 'email', 'is_superuser']
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2' 'user_permissions', 'groups')}
+        ),
+    )
+
+
+admin.site.register(get_user_model(), CustomUserAdmin)
