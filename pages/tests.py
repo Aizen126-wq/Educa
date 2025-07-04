51724b0f4bbd1ef03b67707af573ff765e390fdc
Aizen <eltoncambulo@gmail.com>
2025-07-04 08:19:21 -0300

    tests done

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import HomePageView
 
-# Create your tests here.
+class PageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_home_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_home_page_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_home_page_template(self):
+        self.assertTemplateUsed(self.response, 'index.html')
+        self.assertContains(self.response, 'Pagina Inicial')
+
+    def test_home_page_view(self):
+        view = resolve('/')
+        self.assertEqual(view.func.view_class, HomePageView)
